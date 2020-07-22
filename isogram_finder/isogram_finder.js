00e0e98bc877d61c43cb59d1c5fcc3373fe24c64
peaky76 <robertjamespeacock@gmail.com>
2020-07-22 16:22:32 +0100

    isogram tested and passed

--- isogram_finder/isogram_finder.js
+++ isogram_finder/isogram_finder.js
@@ -1,9 +1,19 @@
 const IsogramFinder = function (word) {
+    this.chars = word.split('');
+};
 
-}
+IsogramFinder.prototype.isIsogram = function() {
+    return this.chars.length === this.getUniqueLetters().length;
+};
 
-IsogramFinder.prototype.isIsogram = function () {
-
-}
+IsogramFinder.prototype.getUniqueLetters = function() {
+    const reducer = function(uniques, letter) {
+        if (!uniques.includes(letter)) {
+            uniques.push(letter);
+        };
+        return uniques;
+    };
+    return this.chars.reduce(reducer, []);
+};
 
 module.exports = IsogramFinder;
