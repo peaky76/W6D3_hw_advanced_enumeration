29733928e6748acc3b4a7debad922dece7e8dca5
peaky76 <robertjamespeacock@gmail.com>
2020-07-22 17:52:24 +0100

    anagram tested and passed

--- anagram_finder/anagram_finder.js
+++ anagram_finder/anagram_finder.js
@@ -1,9 +1,28 @@
 const AnagramFinder = function (word) {
-
+    this.word = word;
 }
 
 AnagramFinder.prototype.findAnagrams = function (otherWords) {
+    return otherWords.filter( word => this.isAnagram(word) );
+};
 
+AnagramFinder.prototype.isAnagram = function (word) {
+    if (this.isSameWord(word) || this.isDifferentLength(word)) {
+        return false;
+    } 
+    target_chars = sortedChars(this.word);
+    test_chars = sortedChars(word);
+    return test_chars.every( (char, index) => char === target_chars[index] );
 }
+
+AnagramFinder.prototype.isSameWord = function (word) {
+    return this.word === word;
+}
+
+AnagramFinder.prototype.isDifferentLength = function (word) {
+    return this.word.length != word.length
+}
+
+const sortedChars = word => word.toLowerCase().split('').sort();
 
 module.exports = AnagramFinder;
