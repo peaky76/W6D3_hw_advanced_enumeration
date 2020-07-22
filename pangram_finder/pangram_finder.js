388acfd2bcf5374d1b065d4ee59afa3a814db66b
peaky76 <robertjamespeacock@gmail.com>
2020-07-22 15:38:40 +0100

    uppercase and pangramfinder pass

--- pangram_finder/pangram_finder.js
+++ pangram_finder/pangram_finder.js
@@ -1,9 +1,10 @@
 const PangramFinder = function (phrase) {
   this.alphabet = 'qwertyuiopasdfghjklzxcvbnm'.split('');
+  this.phrase_chars = phrase.toLowerCase().split(''); 
 }
 
 PangramFinder.prototype.isPangram = function () {
-
+  return this.alphabet.every( letter => this.phrase_chars.includes(letter) );
 }
 
 module.exports = PangramFinder;
